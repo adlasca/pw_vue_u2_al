61cfe7ed68493858215db9c87322fdeae59b7010
Angelo Lascano <adlascano@uce.edu.ec>
2026-01-08 19:37:37 -0500

    CICLO DE VIDA DE COMPONENTES

--- src/components/PokemonImagen.vue
+++ src/components/PokemonImagen.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="pokeImg-container">
-    <img
-      :src="srcImagen"
-      alt="Pokemon Image"
-    />
+    <img :src="srcImagen" alt="Pokemon Image" />
   </div>
 </template>
 
 <script>
 export default {
-    props:{
-        pokemonId:{
-            type: Number,
-            required:true,
-        }
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
     },
-    computed:{
-        srcImagen(){
-            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
-        }
-        //backpicks (` argumento `) para llamar cualquier variable JS dentro de una cadena con ${}
-    }
+  },
+  computed: {
+    srcImagen() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+    //backpicks (` argumento `) para llamar cualquier variable JS dentro de una cadena con ${}
+  },
+    beforeCreate() {
+    console.log("Apenas inicia la instancia del componente");
+  },
+  created() {
+    console.log("Se resolvieron data, computed, methods y watchers");
+  },
+  beforeUnmount() {
+    console.log("Antes de destruir el componente");
+  },
+  unmounted() {
+    console.log("El componente ha sido destruido completamente");
+  },
 };
 </script>
 
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="poke-container">
     <h1>Pokedex</h1>
-    <PokemonImagen :pokemonId="pokeGanador" />
+    <PokemonImagen v-if="mostrar" :pokemonId="pokeGanador" />
     <PokemonOpciones
       @pokeSeleccionado="evaluarGanador($event)"
       :pokeLista="pokeArr"
     />
-    
   </div>
   <div class="poke-msj">
     <h1>{{ msj }}</h1>
     <button @click="iniciarJuego()">Iniciar</button>
+    <button @click="destruir()">Destruir</button>
   </div>
 </template>
 
@@ -31,27 +31,59 @@ export default {
     return {
       pokeArr: [],
       pokeGanador: null,
-      msj:null
+      msj: null,
+      mostrar: true,
     };
   },
   //mounted: Se ejecuta despues de que se renderiza el componente
+  //Fases del ciclo de vida de un componente
+  //1. Creacion
+  beforeCreate() {
+    console.log("Apenas inicia la instancia del componente");
+  },
+  created() {
+    console.log("Se resolvieron data, computed, methods y watchers");
+  },
+  //2. Montaje: Se renderiza el componente
+  beforeMount() {
+    console.log("Antes de renderizar el componente");
+  },
   mounted() {
     this.iniciarJuego();
   },
+  //3. Actualizacion: Se actualiza el DOM
+  //beforeUpdate: Se ejecuta antes de que el DOM se actualice, sufra algun cambio en el data o props
+  beforeUpdate() {
+    console.log(
+      "beforeUpdate:Cambio de data o props, antes de re-renderizar el DOM"
+    );
+  },
+  updated() {
+    console.log("Updated: Se actualizo tras la re-renderizacion del DOM");
+  },
+  //4. Desmontaje: Se desmonta el componente
+
   methods: {
     async iniciarJuego() {
       this.pokeArr = await obtenerVectorPokemonFacade();
       this.pokeGanador = this.pokeArr[obtenerAleatorioFacade(0, 3)].id;
-        this.msj=null;
+      this.msj = null;
+        this.mostrar = true;
     },
     evaluarGanador(idGanador) {
       console.log("valor desde padre:");
       console.log(idGanador);
       if (idGanador === this.pokeGanador) {
-        this.msj="¡Ganaste!";
+        this.msj = "¡Ganaste!";
       } else {
-        this.msj="¡Perdiste!";
+        this.msj = "¡Perdiste!";
       }
+    },
+    destruir() {
+      this.mostrar = false;
+    },
+    crear() {
+      this.mostrar = true;
     },
   },
 };
@@ -76,7 +108,7 @@ h1 {
 .poke-msj {
   margin-top: 20px;
   text-align: center;
-  color:aqua
+  color: aqua;
 }
 button {
   background-color: rgb(22, 158, 101);
